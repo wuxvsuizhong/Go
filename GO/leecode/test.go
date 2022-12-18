d2e4ebf0057cfffd5224afb5cd0a5a2a5a2af076
devuser01 <devuser01@123.com>
2022-12-18 20:02:58 +0800

    20221218提交2

--- GO/leecode/test.go
+++ GO/leecode/test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "container/list"
-  "fmt"
-  // "strings"
+	"container/list"
+	"fmt"
+	// "strings"
 )
 
 // func called1() (msg string, err error){
@@ -12,81 +12,81 @@ import (
 // }
 
 func main() {
-  type pos [2]int
-  a := pos{4, 5}
-  b := pos{4, 5}
-  // fmt.Printf("%#v\n", a == b)
-  fmt.Println(a == b) //true
+	type pos [2]int
+	a := pos{4, 5}
+	b := pos{4, 5}
+	// fmt.Printf("%#v\n", a == b)
+	fmt.Println(a == b) //true
 
-  num := 5
-  s := make([]bool, num)
-  fmt.Println(s) //[false false false false false]]
+	num := 5
+	s := make([]bool, num)
+	fmt.Println(s) //[false false false false false]]
 
-  m1 := make(map[int]bool, num)
-  m1[1] = true
-  fmt.Println(m1[6]) //false
+	m1 := make(map[int]bool, num)
+	m1[1] = true
+	fmt.Println(m1[6]) //false
 
-  //tmpval := 0
-  //for i := 0; i < 4; i++ {
-  //	fmt.Scanln(&tmpval)
-  //}
+	//tmpval := 0
+	//for i := 0; i < 4; i++ {
+	//	fmt.Scanln(&tmpval)
+	//}
 
-  const str = "hello,gol"
-  fmt.Println("len str", len(str))       //9
-  fmt.Println("len str[:]", len(str[:])) //9
+	const str = "hello,gol"
+	fmt.Println("len str", len(str))       //9
+	fmt.Println("len str[:]", len(str[:])) //9
 
-  var aa byte = 1 << len(str) / 128
-  var bb byte = 1 << len(str[:]) / 128
-  fmt.Println("aa", aa) //4
-  fmt.Println("bb", bb) //0
+	var aa byte = 1 << len(str) / 128
+	var bb byte = 1 << len(str[:]) / 128
+	fmt.Println("aa", aa) //4
+	fmt.Println("bb", bb) //0
 
-  s1 := []int{1, 2, 3, 4, 5}
-  fmt.Println(s1)     //[1 2 3 4 5]
-  s1 = s1[:len(s1)-1] // 相当于pop最后一个元素
-  fmt.Println(s1)     // [1 2 3 4]
+	s1 := []int{1, 2, 3, 4, 5}
+	fmt.Println(s1)     //[1 2 3 4 5]
+	s1 = s1[:len(s1)-1] // 相当于pop最后一个元素
+	fmt.Println(s1)     // [1 2 3 4]
 
-  //var str_list []string
-  //tmp_s := ""
-  //for {
-  //	n, _ := fmt.Scanln(&tmp_s)
-  //	//if tmp_s == "" {
-  //	fmt.Println("n:", n, "tmp_s:", tmp_s)
-  //	//break
-  //	//}
-  //	str_list = append(str_list, tmp_s)
-  //}
+	//var str_list []string
+	//tmp_s := ""
+	//for {
+	//	n, _ := fmt.Scanln(&tmp_s)
+	//	//if tmp_s == "" {
+	//	fmt.Println("n:", n, "tmp_s:", tmp_s)
+	//	//break
+	//	//}
+	//	str_list = append(str_list, tmp_s)
+	//}
 
-  fmt.Println(string(rune('A' + 1))) //'B’
-  // go内置的链表数据结构
-  l1 := list.List{}
-  type node struct {
-    s []rune
-    n int
-  }
-  tmp_v := []rune("0000")
-  //tmp_v[1] += 1
-  for i := 0; i < 10; i++ {
-    tmp_v[i%3] += 1
-    l1.PushBack(node{tmp_v, i})
+	fmt.Println(string(rune('A' + 1))) //'B’
+	// go内置的链表数据结构
+	l1 := list.List{}
+	type node struct {
+		s []rune
+		n int
+	}
+	tmp_v := []rune("0000")
+	//tmp_v[1] += 1
+	for i := 0; i < 10; i++ {
+		tmp_v[i%3] += 1
+		l1.PushBack(node{tmp_v, i})
 
-    //	虽然有循环，但是tmp_v 是一个切片，它是一片数组的映射，所以始终只有一份，所以无论循环多少次，映射的内容以最后一次为准
-    //	要想每次都是单独的切片，那么需要使用切片的拷贝: var tmpCopy []rune; copy(tmpCopy,tmp_v)
+		//	虽然有循环，但是tmp_v 是一个切片，它是一片数组的映射，所以始终只有一份，所以无论循环多少次，映射的内容以最后一次为准
+		//	要想每次都是单独的切片，那么需要使用切片的拷贝: var tmpCopy []rune; copy(tmpCopy,tmp_v)
 
-    //tmpCopy := []rune("0000")
-    //copy(tmpCopy, tmp_v)
-    //l1.PushBack(node{tmpCopy, i})
-  }
+		//tmpCopy := []rune("0000")
+		//copy(tmpCopy, tmp_v)
+		//l1.PushBack(node{tmpCopy, i})
+	}
 
-  for e := l1.Front(); e != nil; e = e.Next() {
-    fmt.Println(e.Value)
-    /*
-    	切片中的值都一样，以最后一次的值为准
-    	{[52 51 51 48] 0}
-    	{[52 51 51 48] 1}
-    	......
-    	{[52 51 51 48] 8}
-    	{[52 51 51 48] 9}
-    */
-  }
+	for e := l1.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+		/*
+			切片中的值都一样，以最后一次的值为准
+			{[52 51 51 48] 0}
+			{[52 51 51 48] 1}
+			......
+			{[52 51 51 48] 8}
+			{[52 51 51 48] 9}
+		*/
+	}
 
 }
--- GO/leecode/钱币兑换.go
+++ GO/leecode/钱币兑换.go
@@ -3,108 +3,108 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strconv"
-  "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main2() {
-  scanner := bufio.NewScanner(os.Stdin)
-  scanner.Split(bufio.ScanLines)
-  var snumSlice = []string{}
-  for scanner.Scan() {
-    if scanner.Text() == "\r" {
-      break
-    }
-    snumSlice = append(snumSlice, (strings.Split(strings.TrimSpace(scanner.Text()), " "))...)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+	var snumSlice = []string{}
+	for scanner.Scan() {
+		if scanner.Text() == "\r" {
+			break
+		}
+		snumSlice = append(snumSlice, (strings.Split(strings.TrimSpace(scanner.Text()), " "))...)
 
-  }
+	}
 
-  fmt.Printf("%#v\n", snumSlice)
+	fmt.Printf("%#v\n", snumSlice)
 }
 
 func toInt(s []string) (inums []int) {
-  results := []int{}
-  for _, val := range s {
-    n, _ := strconv.Atoi(val)
-    results = append(results, n)
-  }
-  return results
+	results := []int{}
+	for _, val := range s {
+		n, _ := strconv.Atoi(val)
+		results = append(results, n)
+	}
+	return results
 }
 
 func getMin(nums *[]int) int {
-  if len(*nums) == 0 {
-    return 0
-  }
-  var min int = (*nums)[0]
-  for _, v := range *nums {
-    if v < min {
-      min = v
-    }
-  }
-  return min
+	if len(*nums) == 0 {
+		return 0
+	}
+	var min int = (*nums)[0]
+	for _, v := range *nums {
+		if v < min {
+			min = v
+		}
+	}
+	return min
 }
 
 func coinsProcess(total int, coins *map[int]interface{}) int {
-  // countArr := make([]int, total+1)
-  countArr := make(map[int]int)
-  coinList := []int{}
-  for v, _ := range *coins {
-    countArr[v] = 1
-    coinList = append(coinList, v)
-  }
+	// countArr := make([]int, total+1)
+	countArr := make(map[int]int)
+	coinList := []int{}
+	for v, _ := range *coins {
+		countArr[v] = 1
+		coinList = append(coinList, v)
+	}
 
-  // hascoin := func(k int) bool {
-  // 	if _, ok := (*coins)[k]; ok {
-  // 		return true
-  // 	} else {
-  // 		return false
-  // 	}
-  // }
+	// hascoin := func(k int) bool {
+	// 	if _, ok := (*coins)[k]; ok {
+	// 		return true
+	// 	} else {
+	// 		return false
+	// 	}
+	// }
 
-  for i := 1; i <= total; i++ {
-    if i < getMin(&coinList) {
-      countArr[i] = 0
-      continue
-    }
+	for i := 1; i <= total; i++ {
+		if i < getMin(&coinList) {
+			countArr[i] = 0
+			continue
+		}
 
-    arr := []int{}
-    for v, _ := range *coins {
-      ret := i - v
-      if (ret == 0) || (ret > 0 && countArr[ret] != 0) { //可细分
-        arr = append(arr, countArr[ret]+1)
-      }
-    }
-    fmt.Println("i", i, "arr", arr, "getMin:", getMin(&arr))
-    // fmt.Println(arr)
-    countArr[i] = getMin(&arr)
-  }
-  fmt.Println(countArr)
-  fmt.Println(countArr[total])
-  if countArr[total] == 0 {
-    return -1
-  } else {
-    return countArr[total]
-  }
+		arr := []int{}
+		for v, _ := range *coins {
+			ret := i - v
+			if (ret == 0) || (ret > 0 && countArr[ret] != 0) { //可细分
+				arr = append(arr, countArr[ret]+1)
+			}
+		}
+		fmt.Println("i", i, "arr", arr, "getMin:", getMin(&arr))
+		// fmt.Println(arr)
+		countArr[i] = getMin(&arr)
+	}
+	fmt.Println(countArr)
+	fmt.Println(countArr[total])
+	if countArr[total] == 0 {
+		return -1
+	} else {
+		return countArr[total]
+	}
 
 }
 
 func main() {
-  ireader := bufio.NewReader(os.Stdin)
-  bytes, _, _ := ireader.ReadLine()
-  // fmt.Printf("%#v\n", string(bytes))
-  nums := toInt(strings.Split(strings.TrimSpace(string(bytes)), " "))
-  fmt.Printf("%#v\n", nums)
-  coins := make(map[int]interface{}, len(nums))
-  var mem interface{}
-  for _, v := range nums {
-    coins[v] = mem
-  }
-  bytes, _, _ = ireader.ReadLine()
-  total := toInt(strings.Split(strings.TrimSpace(string(bytes)), " "))[0]
-  fmt.Printf("%#v\n", total)
+	ireader := bufio.NewReader(os.Stdin)
+	bytes, _, _ := ireader.ReadLine()
+	// fmt.Printf("%#v\n", string(bytes))
+	nums := toInt(strings.Split(strings.TrimSpace(string(bytes)), " "))
+	fmt.Printf("%#v\n", nums)
+	coins := make(map[int]interface{}, len(nums))
+	var mem interface{}
+	for _, v := range nums {
+		coins[v] = mem
+	}
+	bytes, _, _ = ireader.ReadLine()
+	total := toInt(strings.Split(strings.TrimSpace(string(bytes)), " "))[0]
+	fmt.Printf("%#v\n", total)
 
-  fmt.Println(coinsProcess(total, &coins))
+	fmt.Println(coinsProcess(total, &coins))
 }
